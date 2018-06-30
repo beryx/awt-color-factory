28dfe82ef658dd869fe22276e739d84adebcd5e2
Serban Iordache <serban.iordache@scoop-software.de>
2018-06-30 13:08:03 +0200

    initial implementation

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Repository created by BootHub
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://github.com/beryx/awt-color-factory/blob/master/LICENSE)
+[![Build Status](https://img.shields.io/travis/beryx/awt-color-factory/master.svg?label=Build)](https://travis-ci.org/beryx/awt-color-factory)
+
+# AWT Color Factory
+
+A utility class that offers methods for creating java.awt.Color instances from string representations.
+
--- src/test/java/org/beryx/awt/color/AwtColorFactoryTest.java
+++ src/test/java/org/beryx/awt/color/AwtColorFactoryTest.java
@@ -1,28 +1,43 @@
 /*
  * Copyright (c) 2018 the original author or authors
- * 
- *     This program is free software; you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation; either version 2 of the License, or
- *     (at your option) any later version.
- * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- * 
- *     You should have received a copy of the GNU General Public License along
- *     with this program; if not, write to the Free Software Foundation, Inc.,
- *     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation. The authors designate this
+ * particular file as subject to the "Classpath" exception as provided
+ * by the authors in the LICENSE file that accompanied this code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 package org.beryx.awt.color;
 
 import org.junit.Test;
+
+import java.awt.*;
+
 import static org.junit.Assert.assertEquals;
 
 public class AwtColorFactoryTest {
     @Test
-    public void onePlusOneShouldEqualTwo() {
-        assertEquals(2, 1 + 1);
+    public void valueOfTesz() {
+        assertEquals(Color.red, ColorFactory.valueOf("red"));
+        assertEquals(ColorFactory.FIREBRICK, ColorFactory.valueOf("firebrick"));
+        assertEquals(new Color(170, 56, 224), ColorFactory.valueOf("#aa38e0"));
+        assertEquals(new Color(64, 168, 204), ColorFactory.valueOf("0x40A8CC"));
+        assertEquals(new Color(112, 36, 228, (int)(0.9 * 255 + 0.5)), ColorFactory.valueOf("rgba(112,36,228,0.9)"));
+        assertEquals(new Color(128, 0, 255), ColorFactory.valueOf("hsla(270,100%,100%,1.0)"));
+    }
+
+    @Test
+    public void webTest() {
+        assertEquals(new Color(170, 56, 224, (int)(0.3 * 255 + 0.5)), ColorFactory.web("#aa38e0", 0.3));
+        assertEquals(new Color(64, 168, 204, (int)(0.7 * 255 + 0.5)), ColorFactory.web("0x40A8CC", 0.7));
     }
 }
